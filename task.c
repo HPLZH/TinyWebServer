01c3a474f4a4d07e6d2c865c5293f9d399adb4c0
李子晗 <lizihan@hplzh.cn>
2025-03-15 12:45:08 +0800

    code format

--- task.c
+++ task.c
@@ -6,19 +6,19 @@
 #define WSA_GET_ERROR errno
 #endif
 
-void task(TARGS args) 
+void task(TARGS args)
 {
 	SOCKET s = args.socket;
 	char buf[1024];
 	int len;
 
-	char* dp = NULL;
-	char* rp = NULL;
+	char *dp = NULL;
+	char *rp = NULL;
 
-	char* rdata = NULL;
+	char *rdata = NULL;
 	int rsize = 0;
 
-	char* sdata = NULL;
+	char *sdata = NULL;
 	int ssize = 0;
 
 	char method[64];
@@ -27,16 +27,16 @@ void task(TARGS args)
 
 	while (len = recv(s, buf, 1024, 0))
 	{
-		if (len == SOCKET_ERROR) 
+		if (len == SOCKET_ERROR)
 		{
 			printf("Error at recv(): %d\n", WSA_GET_ERROR);
 			break;
 		}
 
-		if (rsize == 0) 
+		if (rsize == 0)
 		{
 			rp = realloc(rdata, (size_t)len + 1);
-			if (rp == NULL) 
+			if (rp == NULL)
 			{
 				if (rdata)
 				{
@@ -63,40 +63,48 @@ void task(TARGS args)
 			rp = realloc(rdata, rsize + len + 1);
 			if (rp == NULL)
 			{
-				if (rdata) free(rdata);
+				if (rdata)
+				{
+					free(rdata);
+				}
 				send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 				closesocket(s);
 				return;
 			}
 			else
+			{
 				rdata = rp;
+			}
 			memcpy(rdata + rsize, buf, len);
 			rsize += len;
 			rdata[rsize] = 0;
 		}
 
-		if (dp = strstr(rdata, "\r\n\r\n")) 
+		if (dp = strstr(rdata, "\r\n\r\n"))
 		{
 			int m = sscanf(rdata, "%s %s %s", method, path, ver);
-			if (m == 3) 
+			if (m == 3)
 			{
 				logRequest(args, method, path);
 				memset(path + strlen(path), 0, 256 - strlen(path));
 				int mr = 0;
-				if (0 == strcmp(method, "GET")) 
+				if (0 == strcmp(method, "GET"))
 				{
 					mr = 1;
 				}
-				else if (0 == strcmp(method, "HEAD")) 
+				else if (0 == strcmp(method, "HEAD"))
 				{
 					mr = 0;
 				}
-				else {
+				else
+				{
 					ssize = MethodNotAllowed(&sdata);
-					if (ssize == -1) 
+					if (ssize == -1)
 					{
-						if (rdata) free(rdata);
-						if (sdata) free(sdata);
+						if (rdata)
+							free(rdata);
+						if (sdata)
+							free(sdata);
 						send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 						closesocket(s);
 						return;
@@ -108,11 +116,11 @@ void task(TARGS args)
 					break;
 				}
 				int chcode = 0;
-				const char* ppath = path;
-				if (0 == strncmp(path, "/@/", 3)) 
+				const char *ppath = path;
+				if (0 == strncmp(path, "/@/", 3))
 				{
 					ppath += 6;
-					if (*ppath != '/') 
+					if (*ppath != '/')
 					{
 						ppath = path;
 					}
@@ -122,34 +130,34 @@ void task(TARGS args)
 						memcpy(tmpbuf, path + 3, 3);
 						tmpbuf[3] = 0;
 						chcode = atoi(tmpbuf);
-						if (chcode == 0) 
+						if (chcode == 0)
 						{
 							ppath = path;
 						}
 					}
 				}
 				int fcr = fileCheck(buf, ppath);
-				FILE* fc = NULL;
+				FILE *fc = NULL;
 				long fl = 0;
-				if (fcr == 0) 
+				if (fcr == 0)
 				{
 					fc = fopen(buf, "rb");
-					const char* ctype;
-					if (fc == NULL) 
+					const char *ctype;
+					if (fc == NULL)
 					{
 						fcr = -500;
 						ssize = InternalServerError(&sdata);
-						if (ssize == -1) 
+						if (ssize == -1)
 						{
-							if (fc) 
+							if (fc)
 							{
 								fclose(fc);
 							}
-							if (rdata) 
+							if (rdata)
 							{
 								free(rdata);
 							}
-							if (sdata) 
+							if (sdata)
 							{
 								free(sdata);
 							}
@@ -171,17 +179,17 @@ void task(TARGS args)
 						{
 							ssize = HttpCode(&sdata, chcode, buf);
 						}
-						if (ssize == -1) 
+						if (ssize == -1)
 						{
-							if (fc) 
+							if (fc)
 							{
 								fclose(fc);
 							}
-							if (rdata) 
+							if (rdata)
 							{
 								free(rdata);
 							}
-							if (sdata) 
+							if (sdata)
 							{
 								free(sdata);
 							}
@@ -199,41 +207,69 @@ void task(TARGS args)
 						}
 					}
 				}
-				else if (fcr == -403) 
+				else if (fcr == -403)
 				{
 					ssize = Forbidden(&sdata);
-					if (ssize == -1) 
+					if (ssize == -1)
 					{
-						if (fc) fclose(fc);
-						if (rdata) free(rdata);
-						if (sdata) free(sdata);
+						if (fc)
+						{
+							fclose(fc);
+						}
+						if (rdata)
+						{
+							free(rdata);
+						}
+						if (sdata)
+						{
+							free(sdata);
+						}
 						send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 						closesocket(s);
 						return;
 					}
 					logResponse(args, HTTP_403);
 				}
-				else if (fcr == -404) 
+				else if (fcr == -404)
 				{
 					ssize = NotFound(&sdata);
-					if (ssize == -1) 
+					if (ssize == -1)
 					{
-						if (fc) fclose(fc);
-						if (rdata) free(rdata);
-						if (sdata) free(sdata);
+						if (fc)
+						{
+							fclose(fc);
+						}
+						if (rdata)
+						{
+							free(rdata);
+						}
+						if (sdata)
+						{
+							free(sdata);
+						}
 						send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 						closesocket(s);
 						return;
 					}
 					logResponse(args, HTTP_404);
 				}
-				else {
+				else
+				{
 					ssize = InternalServerError(&sdata);
-					if (ssize == -1) 
+					if (ssize == -1)
 					{
-						if (fc) fclose(fc);
-						if (rdata) free(rdata);
-						if (sdata) free(sdata);
+						if (fc)
+						{
+							fclose(fc);
+						}
+						if (rdata)
+						{
+							free(rdata);
+						}
+						if (sdata)
+						{
+							free(sdata);
+						}
 						send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 						closesocket(s);
 						return;
@@ -241,7 +277,7 @@ void task(TARGS args)
 					logResponse(args, HTTP_500);
 				}
 				send(s, sdata, ssize, 0);
-				if (fcr == 0 && mr) 
+				if (fcr == 0 && mr)
 				{
 					size_t scount = 0;
 					size_t slen = 0;
@@ -250,14 +286,16 @@ void task(TARGS args)
 						send(s, buf, slen, 0);
 						scount += slen;
 						if (slen != 1024)
+						{
 							break;
+						}
 					}
-					if (scount < fl) 
+					if (scount < fl)
 					{
 						memset(buf, 0, 1024);
 						while (scount < fl)
 						{
-							if (fl - scount < 1024) 
+							if (fl - scount < 1024)
 							{
 								send(s, buf, fl - scount, 0);
 								break;
@@ -281,10 +319,16 @@ void task(TARGS args)
 			{
 				logRequest(args, "NULL", "Bad Request");
 				ssize = BadRequest(&sdata);
-				if (ssize == -1) 
+				if (ssize == -1)
 				{
-					if (rdata) free(rdata);
-					if (sdata) free(sdata);
+					if (rdata)
+					{
+						free(rdata);
+					}
+					if (sdata)
+					{
+						free(sdata);
+					}
 					send(s, RESPONSE_500, RESPONSE_500_LEN, 0);
 					closesocket(s);
 					return;
@@ -307,70 +351,69 @@ void task(TARGS args)
 	rsize = 0;
 }
 
-void logRequest(TARGS targs, const char* method, const char* path) 
+void logRequest(TARGS targs, const char *method, const char *path)
 {
 	time_t curt = time(NULL);
-	struct tm* p;
+	struct tm *p;
 	IN_ADDR addr;
 	memcpy(&addr, targs.sockaddr.sa_data + 2, sizeof(addr));
 	p = localtime(&curt);
 
 	printf("[%04d-%02d-%02d %02d:%02d:%02d] IP %-16s %-8s  %s\n",
-		1900 + p->tm_year,
-		1 + p->tm_mon,
-		p->tm_mday,
-		p->tm_hour,
-		p->tm_min,
-		p->tm_sec,
-		inet_ntoa(addr),
-		method,
-		path);
+		   1900 + p->tm_year,
+		   1 + p->tm_mon,
+		   p->tm_mday,
+		   p->tm_hour,
+		   p->tm_min,
+		   p->tm_sec,
+		   inet_ntoa(addr),
+		   method,
+		   path);
 
-	if (logFile) 
+	if (logFile)
 	{
 		fprintf(logFile, "[%04d-%02d-%02d %02d:%02d:%02d] IP %-16s %-8s  %s\n",
-			1900 + p->tm_year,
-			1 + p->tm_mon,
-			p->tm_mday,
-			p->tm_hour,
-			p->tm_min,
-			p->tm_sec,
-			inet_ntoa(addr),
-			method,
-			path);
+				1900 + p->tm_year,
+				1 + p->tm_mon,
+				p->tm_mday,
+				p->tm_hour,
+				p->tm_min,
+				p->tm_sec,
+				inet_ntoa(addr),
+				method,
+				path);
 		fflush(logFile);
 	}
 }
 
-void logResponse(TARGS targs, const char* response) 
+void logResponse(TARGS targs, const char *response)
 {
 	time_t curt = time(NULL);
-	struct tm* p;
+	struct tm *p;
 	IN_ADDR addr;
 	memcpy(&addr, targs.sockaddr.sa_data + 2, sizeof(addr));
 	p = localtime(&curt);
 
 	printf("[%04d-%02d-%02d %02d:%02d:%02d] IP %-16s RESPONSE  %s\n",
-		1900 + p->tm_year,
-		1 + p->tm_mon,
-		p->tm_mday,
-		p->tm_hour,
-		p->tm_min,
-		p->tm_sec,
-		inet_ntoa(addr),
-		response);
-	if (logFile) 
+		   1900 + p->tm_year,
+		   1 + p->tm_mon,
+		   p->tm_mday,
+		   p->tm_hour,
+		   p->tm_min,
+		   p->tm_sec,
+		   inet_ntoa(addr),
+		   response);
+	if (logFile)
 	{
 		fprintf(logFile, "[%04d-%02d-%02d %02d:%02d:%02d] IP %-16s RESPONSE  %s\n",
-			1900 + p->tm_year,
-			1 + p->tm_mon,
-			p->tm_mday,
-			p->tm_hour,
-			p->tm_min,
-			p->tm_sec,
-			inet_ntoa(addr),
-			response);
+				1900 + p->tm_year,
+				1 + p->tm_mon,
+				p->tm_mday,
+				p->tm_hour,
+				p->tm_min,
+				p->tm_sec,
+				inet_ntoa(addr),
+				response);
 		fflush(logFile);
 	}
-
 }
