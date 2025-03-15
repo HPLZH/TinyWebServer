5addfb684f3b53bad6feeb490f6c34bb8d0d656a
李子晗 <lizihan@hplzh.cn>
2025-03-15 13:31:28 +0800

    bugfix linux encoding

--- linuxencoding.c
+++ linuxencoding.c
@@ -3,18 +3,28 @@
 void LinuxUtf8ToGbk(char *dst_str, const char *src_str, size_t inLen, size_t outLen)
 {
     iconv_t cd;
-    char buf[1024];
+    size_t inL = inLen;
+    size_t outL = outLen;
+    char buf[1024] = {0};
+    char obuf[1024] = {0};
     strcpy(buf, src_str);
-    char **pin = (char **)(&buf);
-    char **pout = &dst_str;
+    char *pib = buf;
+    char *pob = obuf;
+    char **pin = &pib;
+    char **pout = &pob;
 
     cd = iconv_open("gbk", "utf8");
     if (cd == 0)
+    {
         return;
-    if (iconv(cd, pin, &inLen, pout, &outLen) == -1)
+    }
+    if (iconv(cd, pin, &inL, pout, &outL) == -1)
+    {
         return;
+    }
+    strncpy(dst_str, obuf, outL);
     iconv_close(cd);
-    *pout = '\0';
+    *pout = 0;
 
     return;
 }
